5397556ec5348309276870ad7a5993075c337476
kevin <krarvin1000@gmail.com>
2022-02-25 08:54:29 +1300

    initial commit-project structure setup

---
+++ src/__tests__/add.test.ts
@@ -0,0 +1,13 @@
+import add from '@src/math/add'
+
+describe('this is a test', () =>{
+    it('should pass', () => {
+        expect(add(1,2)).toBe(7);
+    });
+});
+
+describe('this is a test', () =>{
+    it('should pass', () => {
+        expect(add(1,2)).toBe(3);
+    });
+});
